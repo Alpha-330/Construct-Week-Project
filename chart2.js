55ea1541e12fe2f883c5551e4c1b3274703c6da2
Ranjeet Vishwakarma <52901207+Ranjeet7875@users.noreply.github.com>
2024-10-04 16:12:50 +0530

    Graph

---
+++ chart2.js
@@ -0,0 +1,20 @@
+const ctx2 = document.getElementById('doughnut');
+
+  new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+      labels: ['0.4:47 01 Oct', '06:00 02 Oct', '0.80:00 01 Oct', '09:00 01 Oct', '10:00 01 Oct', '12.23: 01 Oct'],
+      datasets: [{
+        label: 'TaskView by Users ',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
